b2eef1d8b1e7bb8633d661658eb6f8de96b4d22a
ruben.wagner.de@googlemail.com <ruben.wagner.de@googlemail.com@b42d71ec-3d02-2106-bd00-a79425298d90>
2011-09-27 08:24:06 +0000

    fix a bug, which causes the helper not to show the error message refactor to output buffering

--- trunk/src/main/php/De/Aktey/Secapp/Application/Security/SigninViewHelper.php
+++ trunk/src/main/php/De/Aktey/Secapp/Application/Security/SigninViewHelper.php
@@ -3,24 +3,27 @@
 class De_Aktey_Secapp_Application_Security_SigninViewHelper extends Zend_View_Helper_Abstract {
 
 	public function signin() {
-		return '<form method="post" action="'. $this->view->url(array('action' => 'login')) .'">
-				<table id="secapp_login">
-					'. ($this->view->fail ? '
-						<tr>
-							<td id="secapp_login_error" colspan="2">'. $this->view->escape($this->fail) .'</td>
-						</tr>' : '' ) .'
+		ob_start(); ?>
+		<form method="post" action="<?= $this->view->url(array('action' => 'login')) ?>">
+			<table id="secapp_login">
+				<?php if ($this->view->fail) : ?>
 					<tr>
-						<td><label for="secapp_login_user">Username:</lable></td>
-						<td><input id="secapp_login_user" type="text" name="username" /></td>
+						<td id="secapp_login_error" colspan="2"><?= $this->view->escape($this->view->fail) ?></td>
 					</tr>
-					<tr>
-						<td><label for="secapp_login_passwd">Password:</lable></td>
-						<td><input id="secapp_login_passwd" type="password" name="password" /></td>
-					</tr>
-					<tr>
-                    	<td colspan="2" align="right"><input type="submit" name="login" value="Sign in" /></td>
-					</tr>
-				</table>
-			</form>';
+				<?php endif ?>
+				<tr>
+					<td><label for="secapp_login_user">Username:</lable></td>
+					<td><input id="secapp_login_user" type="text" name="username" /></td>
+				</tr>
+				<tr>
+					<td><label for="secapp_login_passwd">Password:</lable></td>
+					<td><input id="secapp_login_passwd" type="password" name="password" /></td>
+				</tr>
+				<tr>
+                	<td colspan="2" align="right"><input type="submit" name="login" value="Sign in" /></td>
+				</tr>
+			</table>
+		</form>
+		<?php return ob_get_clean();
 	}
 }
